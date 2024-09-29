13c8499513a6db1629e048dc6e943bc72030d8f9
Pushkraj P Naik <140972568+Pushkrajpnaik@users.noreply.github.com>
2024-09-29 18:22:12 +0530

    Update script.js
    Summary of Corrections
    Removed duplicate event handlers.
    Added const and let for variable declarations.
    Adjusted the logic for handling months in the expiration date.
    Ensured all paths in getCreditCardType return a value.
    Cleared unnecessary empty setTimeout function.

--- script.js
+++ script.js
@@ -1,56 +1,44 @@
-$('.input-cart-number').on('keyup change', function(){
-  $t = $(this);
+$('.input-cart-number').on('keyup change', function() {
+  const $t = $(this);
   
   if ($t.val().length > 3) {
-    $t.next().focus();
+    $t.next('.input-cart-number').focus();
   }
   
-  var card_number = '';
-  $('.input-cart-number').each(function(){
+  let card_number = '';
+  $('.input-cart-number').each(function() {
     card_number += $(this).val() + ' ';
-    if ($(this).val().length == 4) {
-      $(this).next().focus();
-    }
-  })
+  });
   
-  $('.credit-card-box .number').html(card_number);
+  $('.credit-card-box .number').html(card_number.trim());
 });
 
-$('#card-holder').on('keyup change', function(){
-  $t = $(this);
+$('#card-holder').on('keyup change', function() {
+  const $t = $(this);
   $('.credit-card-box .card-holder div').html($t.val());
 });
 
-$('#card-holder').on('keyup change', function(){
-  $t = $(this);
-  $('.credit-card-box .card-holder div').html($t.val());
-});
-
-$('#card-expiration-month, #card-expiration-year').change(function(){
-  m = $('#card-expiration-month option').index($('#card-expiration-month option:selected'));
-  m = (m < 10) ? '0' + m : m;
-  y = $('#card-expiration-year').val().substr(2,2);
+$('#card-expiration-month, #card-expiration-year').change(function() {
+  const m = $('#card-expiration-month').val(); // get selected month
+  const y = $('#card-expiration-year').val().substr(2, 2); // get last two digits of year
   $('.card-expiration-date div').html(m + '/' + y);
-})
+});
 
-$('#card-ccv').on('focus', function(){
+$('#card-ccv').on('focus', function() {
   $('.credit-card-box').addClass('hover');
-}).on('blur', function(){
+}).on('blur', function() {
   $('.credit-card-box').removeClass('hover');
-}).on('keyup change', function(){
+}).on('keyup change', function() {
   $('.ccv div').html($(this).val());
 });
 
-
-/*--------------------
-CodePen Tile Preview
---------------------*/
-setTimeout(function(){
-
-  });
+// CodePen Tile Preview
+setTimeout(function() {
+  // Functionality could be added here if needed
 }, 500);
 
-/*function getCreditCardType(accountNumber) {
+function getCreditCardType(accountNumber) {
+  let result;
   if (/^5[1-5]/.test(accountNumber)) {
     result = 'mastercard';
   } else if (/^4/.test(accountNumber)) {
@@ -58,11 +46,11 @@ setTimeout(function(){
   } else if ( /^(5018|5020|5038|6304|6759|676[1-3])/.test(accountNumber)) {
     result = 'maestro';
   } else {
-    result = 'unknown'
+    result = 'unknown';
   }
   return result;
 }
 
-$('#card-number').change(function(){
+$('#card-number').change(function() {
   console.log(getCreditCardType($(this).val()));
-})*/
+});
